11f20ac04b2a2659c9dc908a555c8a8d816fe7de
Calvin Metcalf <cmetcalf@appgeo.com>
2015-05-08 14:21:23 -0400

    commas

--- test/4-delete.js
+++ test/4-delete.js
@@ -4,15 +4,10 @@ var sqlg = require('../');
 exports['test_delete'] = function( test, assert ) {
     if( !assert ) { assert = test; assert.finish = function() { this.done() } }
 
-    assert.deepEqual( { sql: 'DELETE FROM test', values: [] },
+    assert.deepEqual( { sql: 'DELETE FROM test;', values: [] },
                       sqlg.delete( 'test', null ) );
 
-    sqlg.delete( 'test', null, function( sql, values ) {
-        assert.deepEqual( { sql: 'DELETE FROM test', values: [] },
-                          { sql: sql, values: values } );
-    } );
-
-    assert.deepEqual( { sql: 'DELETE FROM test WHERE id = $1', values: [ 10 ] },
+    assert.deepEqual( { sql: 'DELETE FROM test WHERE id = $1;', values: [ 10 ] },
                       sqlg.delete( 'test', { id: 10 } ) );
 
     if( typeof test.finish == 'function' ) test.finish();
